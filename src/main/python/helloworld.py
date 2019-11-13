7a0d3e5bdd9216b01d3aa6823227d93fd92edee8
rakshithrgopidi <rakshithgopidi@gmail.com>
2019-11-13 11:20:10 +0530

    first commit

---
+++ src/main/python/helloworld.py
@@ -0,0 +1,21 @@
+import sys
+#hello
+def helloworld(out):
+	out.write('hello meow')
+	out.write('hello Aparna')
+	print("Hello World")
+
+print("Hello Aman")
+print("Hello Apar")
+print("Hello Shashwat")
+print("Hello Brother")
+print("Hello Abhijit")
+print("YEH!")
+print("Hurray")
+print("Hello sir")
+print("Successful Build")
+print("Good Job Aman")
+print("Hello Raghav")
+print("Hello Mr Bhavesh")
+
+
---
+++ target/dist/pybuilder12-1.0.dev0/build/lib.linux-x86_64-2.7/helloworld.py
@@ -0,0 +1,11 @@
+import sys
+#hello
+def helloworld(out):
+	out.write('hello Aman')
+	out.write('hello Apar')
+	print("Hello World")
+
+print("Hello Aman")
+print("Hello Apar")
+print("Hello Shashwat")
+
